9995be8ba71ba3375678d0707c139b6211317a21
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-02-09 04:10:12 +0000

    done the code

--- p1original.c
+++ p1original.c
@@ -1,1 +1,29 @@
-#include<studio.h>
+#include<stdio.h>
+#include<math.h>
+void input(float *b,float *h)
+{
+  printf("enter the value of base and height\n");
+  scanf("%f %f",&b,&h);
+}
+void find_area(float b,float h,float*area)
+
+{
+   *area=1/2*b*h;
+}
+void output(float base,float height,float area)
+{
+  printf("The area of a triangle is %f",area);
+}
+int main()
+{
+  float x,y,z;
+  input(&x,&y);
+  find_area(x,y,&z);
+  output(x,y,z);
+  return 0;
+
+}
+
+
+
+
