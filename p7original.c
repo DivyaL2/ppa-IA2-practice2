a832302d3927472eef0b7d60a59c2350eb53b9da
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-03-12 17:23:17 +0000

    done the code

--- p7original.c
+++ p7original.c
@@ -2,33 +2,30 @@
 #include<math.h>
 struct _triangle
 {
-  float b,h,area;
+ float b,h ,area ;
 };
 typedef struct _triangle Triangle;
-Triangle input_triangle(Triangle a)
+Triangle input_triangle()
+{
+  Triangle k;
+  printf("enter the value of k\n");
+  scanf("%f %f",&k.b,&k.h);
+  return k;
+}
+Triangle find_area(Triangle k)
 {
   
-  printf("enter the value of a);
-  scanf("%f",&a);
+  k->area=0.5 * k->b * k->h;
 }
-Triangle find_area(Triangle b ,Triangle h)
-{ 
-   Triangle area ;
-   area =(b*h)/2;
-  
-  return area;
-}
-void output(Triangle area)
+void output(Triangle t)
 {
-  printf("the area of a triangle is %f",area);
-
+  printf("The area of a triangle is %f\n",t.area);
 }
 int main()
-{
-  Triangle c,base,height;
-   base =input_triangle();
-   height=input_triangle();
-  c =find_area(a,b);
-  output(a,b,c);
+{   
+   Triangle t;
+  t=input_triangle();
+  find_area(&t);
+  output(t);
   return 0;
-}
+  } 
