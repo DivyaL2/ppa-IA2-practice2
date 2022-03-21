f629940c10c4cc853e29131f842df6c17213404a
DivyaL2 <96220816+DivyaL2@users.noreply.github.com>
2022-03-21 17:12:10 +0000

    done the code

--- p4original.c
+++ p4original.c
@@ -6,36 +6,39 @@ int input_array_size()
   scanf("%d",&a);
   return a;
 }
-void input_array(int n,int a[n])
+void input_array(int n,int *a)
 {
   printf("enter the array values\n");
-  int count =0;
-
-  for(int i=0;i<=a[n];i++)
+  
+  
+  for(int i=0;i<n;i++)
   {
-    if(a[i]%i==0)
-    count++;
+    
     scanf("%d",&a[i]);
   }
 }
-int  sum_composite_numbers(int n,int a[n])
+int  iscomposite_numbers(int n)
 {
-  int sum=0,count=0;
   
-
-  for(int i=1;i<n;i++)
-  {
-    
-    
-
-      if(count>2)
-      {
-      sum=sum+a[i];
-      }
-    
-  }
-  return sum;
+  for(int i=2;i<=n/2;i++)
+    {
+    if(n%i==0)
+    return 1;
+    }  
+  return 0;
 }
+    
+int sum_composite_numbers(int n,int a[n])
+{
+  int sum=0;
+  for(int i=0;i<n;i++)
+    if(iscomposite_numbers(a[i]))
+    {
+      sum=sum+a[i];
+    }
+  return sum;
+  }
+
 void output(int sum)
 {
   printf("The sum of composite numbers is %d",sum);
@@ -47,6 +50,7 @@ int main()
   n=input_array_size();
   int a[n];
   input_array(n,a);
+  iscomposite_numbers(n);
   sum=sum_composite_numbers(n,a);
   output(sum);
   return 0;
